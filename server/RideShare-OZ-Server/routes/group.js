f758c81276e46029fe75704a4c4dfd48f572cf66
maxzhx <maxzhx@gmail.com>
2015-09-03 21:37:07 +1000

    change test url

--- server/RideShare-OZ-Server/routes/group.js
+++ server/RideShare-OZ-Server/routes/group.js
@@ -4,9 +4,6 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  var db = req.db;
-  var collection = db.get('groups');
-
   res.send('respond with a resource');
 
 });
--- server/RideShare-OZ-Server/routes/msg.js
+++ server/RideShare-OZ-Server/routes/msg.js
@@ -4,9 +4,6 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  var db = req.db;
-  var collection = db.get('msgs');
-
   res.send('respond with a resource');
 
 });
