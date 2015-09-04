5b333ef0ef075db89fa8b9a2e43aa66148b3b2d8
maxzhx <maxzhx@gmail.com>
2015-09-04 12:07:13 +1000

    fix auth bug

--- server/RideShare-OZ-Server/app.js
+++ server/RideShare-OZ-Server/app.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req,res,next){
-  if (req.method === 'GET') {
+  if (req.method === 'POST') {
     auth.auth_token(req.body.token, function(doc){
       req.userinfo = doc;
       next();
--- server/RideShare-OZ-Server/authentication.js
+++ server/RideShare-OZ-Server/authentication.js
@@ -3,14 +3,14 @@ var auth = {};
 
 auth.auth_token = function (token, callback) {
 
-
   var url = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + token;
 
+  console.log(url);
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(JSON.parse(body));
-      callback(JSON.parse(body));
     }
+    console.log(JSON.parse(body));
+    callback(JSON.parse(body));
   });
 
   /* return result; */
