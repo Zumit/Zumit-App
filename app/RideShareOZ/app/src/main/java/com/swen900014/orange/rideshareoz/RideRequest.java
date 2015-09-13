2b339ea262323acaf0adada155d0d1313870e738
Sangzhuoyang Yu <yuszy@outlook.com>
2015-09-12 21:19:08 -0700

    New request module.

--- app/RideShareOZ/app/src/main/java/com/swen900014/orange/rideshareoz/RideRequest.java
+++ app/RideShareOZ/app/src/main/java/com/swen900014/orange/rideshareoz/RideRequest.java
@@ -12,14 +12,20 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by yuszy on 9/9/15.
  */
 public class RideRequest
 {
-    private final static String RIDE_URL = "http://144.6.226.237/ride/request";
+    private final static String JOIN_REQUEST_URL = "http://144.6.226.237/ride/request";
     private Ride ride;
+
+    private String lat = "";
+    private String lon = "";
 
     public RideRequest(Ride ride)
     {
@@ -28,7 +34,7 @@ public class RideRequest
 
     public void sendRequest(final Activity activity, String address)
     {
-        RequestQueue queue = Volley.newRequestQueue(activity);
+        //RequestQueue queue = Volley.newRequestQueue(activity);
 
         String url = "https://maps.googleapis.com/maps/api/geocode/json?" +
                 "address=" + address + ",+Australia&" +
@@ -39,9 +45,6 @@ public class RideRequest
                 {
                     public void onResponse(String response)
                     {
-                        String lat = "";
-                        String lon = "";
-
                         try
                         {
                             JSONObject jsonResponse = new JSONObject(response);
@@ -62,12 +65,43 @@ public class RideRequest
                             e.printStackTrace();
                         }
 
-                        String message = "ride_id=" + ride.getRideId() +
+                        StringRequest joinRequest = new StringRequest(Request.Method.POST,
+                                JOIN_REQUEST_URL, new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String s)
+                            {
+                                System.out.println("response: " + s);
+                            }
+                        }, new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError)
+                            {
+                                System.out.println("Sending post failed!");
+                            }
+                        }){
+                            protected Map<String, String> getParams()
+                            {
+                                Map<String, String> params = new HashMap<String, String>();
+                                params.put("username", "user1");
+                                params.put("ride_id", "55e7ed577ea19c92ac2d0911");
+                                params.put("p_lat", lat);
+                                params.put("p_lon", lon);
+
+                                return params;
+                            }
+                        };
+
+                        MyRequest.getInstance(activity).addToRequestQueue(joinRequest);
+
+                        /*String message = "ride_id=" + ride.getRideId() +
                                 "&username=" + "user1" + //passenger.getName
                                 "&s_lat=" + lat + "&s_lon=" + lon;
 
                         PostThread post = new PostThread(RIDE_URL, message);
-                        post.start();
+                        post.start();*/
+
                         // check response, whether it received
                     }
                 },
@@ -79,6 +113,7 @@ public class RideRequest
                     }
                 });
 
-        queue.add(stringRequest);
+        //queue.add(stringRequest);
+        MyRequest.getInstance(activity).addToRequestQueue(stringRequest);
     }
 }
