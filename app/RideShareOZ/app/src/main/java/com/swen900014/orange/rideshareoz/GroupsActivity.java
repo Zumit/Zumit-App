f1e41edcde36edff068354763f8200a2de9ce567
georgeshehata <george.nader@gmail.com>
2015-10-14 21:51:39 +1100

    removed options menu from Groups activity

--- app/RideShareOZ/app/src/main/java/com/swen900014/orange/rideshareoz/GroupsActivity.java
+++ app/RideShareOZ/app/src/main/java/com/swen900014/orange/rideshareoz/GroupsActivity.java
@@ -32,13 +32,13 @@ public class GroupsActivity extends AppCompatActivity
         getIntent();
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
