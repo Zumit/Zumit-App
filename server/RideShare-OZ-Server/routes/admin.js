e347eec6ee1410a5444e113c65fbb9a4385ddd6f
maxzhx <maxzhx@gmail.com>
2015-10-16 17:34:56 +1100

    auth

--- server/RideShare-OZ-Server/routes/admin.js
+++ server/RideShare-OZ-Server/routes/admin.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var Group = require('../models/Group.js');
 
 router.get('/', function(req, res, next) {
 
-  Group.find().populate('members',
-      'username phone driver_license').exec({}, function(err, groups){
-    // res.json(groups);
-    res.render('admin', {
-      title: 'Administrator',
-      groups: groups
-    });
-  });
+  passport.authenticate('local', { successRedirect: '/admin',
+                                   failureRedirect: '/',
+                                   failureFlash: true });
+  // Group.find().populate('members',
+      // 'username phone driver_license').exec({}, function(err, groups){
+    // // res.json(groups);
+    // res.render('admin', {
+      // title: 'Administrator',
+      // groups: groups
+    // });
+  // });
 
 });
 
