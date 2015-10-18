7597d906fb31b1cc02bc7f10db5c3597e944d466
georgeshehata <george.nader@gmail.com>
2015-10-18 16:25:44 +1100

    adjusted user info layout

--- app/RideShareOZ/app/src/main/java/com/swen900014/orange/rideshareoz/views/UserInfoActivity.java
+++ app/RideShareOZ/app/src/main/java/com/swen900014/orange/rideshareoz/views/UserInfoActivity.java
@@ -46,7 +46,7 @@ public class UserInfoActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_info);
+        setContentView(R.layout.userinfoactivity_2);
 
         Intent intent = getIntent();
 
